d7825a31acdb173d544c9dd51b8855cc7207971b
Swadesh Chhetri <swadeshchhetri87@gmail.com>
2024-10-21 08:56:59 +0530

    fix the navigation

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/FlavorDash">
       <Provider store={store}>
         <App />
       </Provider>
